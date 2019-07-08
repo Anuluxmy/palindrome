a0f9c715624479a96fa3b74213310b1ae754f92d
anuluxmy <anu_iv2002@hotmail.com>
2019-07-08 18:11:20 +0100

    README.md

---
+++ README.md
@@ -0,0 +1,27 @@
+# Palindrome
+
+Aim
+-------
+```
+To write a function to calculate the largest palindromic number from the product of two 3-digit numbers (100 to 999).
+```
+Clone repository
+----------
+```
+* git clone https://github.com/Anuluxmy/palindrome.git
+* cd palindrome
+```
+
+To run Palindrome
+---------
+```
+* bundle install
+* cd lib
+* ruby palindrome.rb
+```
+
+To run test
+----
+```
+* rspec
+```
--- lib/palindrome.rb
+++ lib/palindrome.rb
@@ -4,9 +4,9 @@ def palindromeNum
  palindroms = [] # empty array to store the palindrome numbers
   for i in from_num..to_num do #iterate the numbers in range
    for j in from_num..to_num do #iterate the numbers in range
-     k = i * j #storing the poduct of 2 numbers in variable
-     if k.to_s == k.to_s.reverse #check whether the string in equal to the reverse string
-      palindroms << k #storing in array
+     product = i * j #storing the poduct of 2 numbers in variable
+     if product.to_s == product.to_s.reverse #check whether the string in equal to the reverse string
+      palindroms << product #storing in array
      end
     end
   end
